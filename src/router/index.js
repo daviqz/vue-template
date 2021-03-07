4651787a7cb15673c9cc0903a8c949d232c4c0b6
Davi Queiroz <dvbarreira@hotmail.com>
2021-03-07 03:26:47 -0300

    Algumas mudanças + criação da página Register

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Register from '../views/Register.vue'
 
 const routes = [
 	{
 		path: '/',
 		name: 'Home',
 		component: Home
+	},
+    {
+		path: '/register',
+		name: 'Register',
+		component: Register
 	},
 	{
 		path: '/about',
@@ -14,7 +20,8 @@ const routes = [
 		// this generates a separate chunk (about.[hash].js) for this route
 		// which is lazy-loaded when the route is visited.
 		component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-	}
+	},
+	
 ]
 
 const router = createRouter({
---
+++ src/views/Register.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="register">
+		<h1>This is a Register page</h1>
+		<Form msg='fds'/>
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Form from '@/components/Form.vue'
+
+export default {
+    name: 'Register',
+    components: {
+      	Form
+    }
+}
+</script>
