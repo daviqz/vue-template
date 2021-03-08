99e758e5d9999c5ea19414cd1add66c983ce9aeb
Davi Queiroz <dvbarreira@hotmail.com>
2021-03-08 01:03:25 -0300

    body parser substituido pelo próprio express

--- back/src/server.js
+++ back/src/server.js
@@ -1,16 +1,11 @@
 const express = require('express')
 const cors = require('cors')
-const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 app.use(cors())
-app.use(bodyParser.json())
-app.use(
-    bodyParser.urlencoded({
-        extended: true
-    })
-)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.post('/register', (req, res) => {
     console.log(req.body)
     if(req.body.name === '1') {
